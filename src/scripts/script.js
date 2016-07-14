1bfc11d8752c03c95fd23d6ad949c351c58ab780
shgtkshruch <shgtk.shruch@gmail.com>
2016-07-14 16:03:22 +0900

    Complete

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,15 @@
+(() => {
+  'use strict';
 
+  const show = document.getElementById('js-show');
+  const main = document.getElementById('js-main');
+
+  show.addEventListener('click', function() {
+    if (main.classList.contains('menu-open')) {
+      main.classList.remove('menu-open');
+    } else {
+      main.classList.add('menu-open');
+    }
+  });
+
+})();
